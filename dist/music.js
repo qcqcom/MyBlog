6f718033f1571b92dbc0dafcc15e9f12f6211477
qinchengqian <1466417796@qq.com>
2019-02-16 23:45:31 +0800

    Site updated: 2019-02-16 23:45:28

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "PDD洪荒之力",
         artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
+        url: '//music.163.com/outchain/player?type=2&id=33911781&auto=1&height=66',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
       },
       {
