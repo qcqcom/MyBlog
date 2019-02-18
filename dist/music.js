cd57be6ffe89b5496a661f3cde926bad63919f78
qinchengqian <1466417796@qq.com>
2019-02-18 13:52:47 +0800

    Site updated: 2019-02-18 13:52:45

---
+++ dist/music.js
@@ -0,0 +1,63 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: '后来',
+        artist: '刘若英',
+        url: 'http://www.ytmp3.cn/down/48101.mp3',
+       
+      },
+      {
+        name: '夜空中最亮的星',
+        artist: '邓紫棋',
+        url: 'http://www.ytmp3.cn/down/49543.mp3',
+	  },
+	  {
+        name: '光年之外',
+        artist: '邓紫棋',
+        url: 'http://www.ytmp3.cn/down/56587.mp3',
+	  },
+	  {
+        name: '后会无期',
+        artist: '朴树',
+        url: 'http://www.ytmp3.cn/down/33539.mp3',
+	  },
+	  {
+        name: '泡沫',
+        artist: '邓紫棋',
+        url: 'http://www.ytmp3.cn/down/38695.mp3',
+	  },
+	  {
+        name: '游乐场',
+        artist: '谢霆锋',
+        url: 'http://www.ytmp3.cn/down/55966.mp3',
+	  },
+	  {
+        name: '活着Viva',
+        artist: '谢霆锋',
+        url: 'http://www.ytmp3.cn/down/53369.mp3',
+	  },
+	  {
+        name: '17岁',
+        artist: '刘德华',
+        url: 'http://www.ytmp3.cn/down/55191.mp3',
+	  },
+      {
+        name: '[笨小孩 (Live) - live]',
+        artist: '刘德华',
+        url: 'http://www.ytmp3.cn/down/42551.mp3',
+	  },
+	  {
+        name: '冰雨',
+        artist: '刘德华',
+        url: 'http://www.ytmp3.cn/down/58252.mp3',
+	  }, 
+	  {
+        name: '习惯失恋',
+        artist: '容祖儿',
+        url: 'http://www.ytmp3.cn/down/57531.mp3',
+	  },	  
+    ]
+});
